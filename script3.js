195445ca45244494345c7416754fc49218eb2f4a
MichalTheProgrammer <56166511+MichalTheProgrammer@users.noreply.github.com>
2020-05-04 17:18:33 +0200

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,13 +1,9 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
 
-const cellElements = document.querySelectorAll('[data-cell]')
+const cellElements = document.querySelectorAll('div.cell')
 const board_id = document.querySelectorAll('div.board')
-const winningMessageElement = document.getElementById('winningMessage')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let circleTurn
-let turn = 0;
-var cell;
+let cell;
 
 var board = "XO-XO-----O-X------O-X------O-X---------------------------X----------------------"
 var bigboard = "-X----O--"
@@ -31,13 +27,15 @@ for (let i=0; i < bigboard.length; ++i)
 		board_id[i].style.height = "24vh"
 		board_id[i].style.width = "24vh"		
 		board_id[i].innerHTML = ""
-		board_id[i].style.backgroundImage = "url('cross.png')";
+		board_id[i].style.backgroundImage = "url('cross1.png')";
 	}
 	else if (bigboard[i] == "O")
 	{		
 		board_id[i].innerHTML = ""
-		board_id[i].style.backgroundImage = board_id[i].style.backgroundImage = "url('circle.png')";
+		board_id[i].style.backgroundImage = board_id[i].style.backgroundImage = "url('circle1.png')";
 		board_id[i].style.height = "24vh"
 		board_id[i].style.width = "24vh"
 	}
 }
+	board_id[1].style.border = "0px"
+	board_id[6].style.border = "0px"
