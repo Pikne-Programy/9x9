07140dd9e9a2c879bc2521962029abfcaebabdf5
MichalTheProgrammer <56166511+MichalTheProgrammer@users.noreply.github.com>
2020-05-04 17:16:26 +0200

    Update onlinegame.js
    Support for private rooms and some minor changes

--- onlinegame.js
+++ onlinegame.js
@@ -1,3 +1,15 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+let number = getParameterByName('serialNumber');
+
 const ttt_websocket = new WebSocket("ws://85.198.250.135:4780");
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
@@ -9,34 +21,56 @@ let circleTurn
 let turn = 0;
 let cell;
 let clicked_cell
+let rooms = number;
+if(number == "")
+	rooms = "public";
 
 let obj = {
 	"status": 0,
     "method": "JON",
     "params": {
-	room: "public"
+		room: rooms
 	},
     "time": Math.round((new Date()).getTime()/1000)	
 	}
 
-let myJSON = JSON.stringify(obj + "\r\n");
+let VER = 
+{
+    "status": 0,
+    "method": "VER",
+    "params": {
+        "protocolVersion": "v1.0"
+    },
+    "time": Math.round((new Date()).getTime()/1000)
+}
+
     
 ttt_websocket.onopen = function()
 {
-	ttt_websocket.send(JSON.stringify(obj));
+	ttt_websocket.send(JSON.stringify(VER));
+	setTimeout(function(){ ttt_websocket.send(JSON.stringify(obj)); }, 4000);
+	
 }
 
-document.getElementById("info").innerHTML = "Waiting for opponent"
+document.getElementById("info").innerText = "Waiting for opponent"
 	
 ttt_websocket.onmessage = function (event) {
 	let msgrec = event.data;
 	let packet = JSON.parse(msgrec)
+	if (packet["method"] == "VER")
+	{
+		if (VER["params"]["protocolVersion"] != packet["params"]["protocolVersion"])
+		{
+			alert("You are not using the actual version of protocol. It can affect your game")
+		}
+	}
 	if (packet["method"] == "STT")
 	{
-		document.getElementById("your_mark").innerHTML = " " + packet["params"]["you"]
-		document.getElementById("now_playing").innerHTML = " " + packet["params"]["move"]
+		console.log(packet)
+			document.getElementById("your_mark").innerHTML = " " + packet["params"]["you"]
+			document.getElementById("now_playing").innerHTML = " " + packet["params"]["move"]
 			
-	for (let i = 0; i < packet["params"]["bigBoard"].length; ++i) 
+			for (let i = 0; i < packet["params"]["bigBoard"].length; ++i) 
 	{
         if (packet["params"]["bigBoard"][i] == "X") 
 		{
@@ -75,15 +109,15 @@ ttt_websocket.onmessage = function (event) {
 			document.getElementById((9*i)+19).style.opacity = "0";
 			document.getElementById((9*i)+20).style.opacity = "0";}
 			if(i==4){
-			document.getElementById((7*i)).style.opacity = "0";
-			document.getElementById((7*i)+1).style.opacity = "0";
-			document.getElementById((7*i)+2).style.opacity = "0"
-			document.getElementById((7*i)+9).style.opacity = "0";
-			document.getElementById((7*i)+10).style.opacity = "0";
+			document.getElementById((7*i)+2).style.opacity = "0";
+			document.getElementById((7*i)+3).style.opacity = "0";
+			document.getElementById((7*i)+4).style.opacity = "0"
 			document.getElementById((7*i)+11).style.opacity = "0";
-			document.getElementById((7*i)+18).style.opacity = "0";
-			document.getElementById((7*i)+19).style.opacity = "0";
-			document.getElementById((7*i)+20).style.opacity = "0";}
+			document.getElementById((7*i)+12).style.opacity = "0";
+			document.getElementById((7*i)+13).style.opacity = "0";
+			document.getElementById((7*i)+20).style.opacity = "0";
+			document.getElementById((7*i)+21).style.opacity = "0";
+			document.getElementById((7*i)+22).style.opacity = "0";}
 			else if (i==7){
 			document.getElementById((8*i)+1).style.opacity = "0";
 			document.getElementById((8*i)+2).style.opacity = "0";
@@ -143,15 +177,15 @@ ttt_websocket.onmessage = function (event) {
 			document.getElementById((9*i)+19).style.opacity = "0";
 			document.getElementById((9*i)+20).style.opacity = "0";}
 			if(i==4){
-			document.getElementById((7*i)).style.opacity = "0";
-			document.getElementById((7*i)+1).style.opacity = "0";
-			document.getElementById((7*i)+2).style.opacity = "0"
-			document.getElementById((7*i)+9).style.opacity = "0";
-			document.getElementById((7*i)+10).style.opacity = "0";
+			document.getElementById((7*i)+2).style.opacity = "0";
+			document.getElementById((7*i)+3).style.opacity = "0";
+			document.getElementById((7*i)+4).style.opacity = "0"
 			document.getElementById((7*i)+11).style.opacity = "0";
-			document.getElementById((7*i)+18).style.opacity = "0";
-			document.getElementById((7*i)+19).style.opacity = "0";
-			document.getElementById((7*i)+20).style.opacity = "0";}
+			document.getElementById((7*i)+12).style.opacity = "0";
+			document.getElementById((7*i)+13).style.opacity = "0";
+			document.getElementById((7*i)+20).style.opacity = "0";
+			document.getElementById((7*i)+21).style.opacity = "0";
+			document.getElementById((7*i)+22).style.opacity = "0";}
 			else if (i==7){
 			document.getElementById((8*i)+1).style.opacity = "0";
 			document.getElementById((8*i)+2).style.opacity = "0";
@@ -176,18 +210,30 @@ ttt_websocket.onmessage = function (event) {
     }
     for (let i = 0; i < packet["params"]["board"].length; ++i) {
         if (packet["params"]["board"][i] == "X") {
-            document.getElementById(i).classList.add(X_CLASS)
+            document.getElementById(i).classList.add(X_CLASS);
         }
         else if (packet["params"]["board"][i] == "O") {
-            document.getElementById(i).classList.add(CIRCLE_CLASS)
+            document.getElementById(i).classList.add(CIRCLE_CLASS);
         }
     }
 	
 	
 		if (packet["params"]["whoWon"] == packet["params"]["you"])
 		{
-			document.getElementById("info").innerHTML = "Congrats! You won!"
-			ttt_websocket.close(); 
+			document.getElementById("info").innerHTML = "Congrats! You won!";
+
+		}
+		else if 
+		(packet["params"]["whoWon"] == "+")
+		{
+			document.getElementById("info").innerHTML = "It's a draw";
+
+		}
+		else if 
+		(packet["params"]["whoWon"] == "X" && packet["params"]["you"]=="O" || packet["params"]["whoWon"] == "O" && packet["params"]["you"]=="X")
+		{
+			document.getElementById("info").innerHTML = "You lose!";
+
 		}
 		else
 	{
@@ -197,6 +243,13 @@ ttt_websocket.onmessage = function (event) {
 	{
 		for (let i=0; i<9; i++)
 		board_id[i].style.border = "solid white 0.5vh"
+		for (let j=0; j<9; j++)
+		{
+			if(packet["params"]["bigBoard"][j] == "X" || packet["params"]["bigBoard"][j] == "O")
+			{
+				board_id[j].style.border = "0px"
+			}
+		}
 	}
 	else
 	{
@@ -204,9 +257,7 @@ ttt_websocket.onmessage = function (event) {
 	board_id[mar].style.border = "solid white 0.5vh"
 	}
 	cellElements.forEach(cell => { cell.addEventListener('click', make_move)})
-	function make_move(e)
-	{
-		if (packet["params"]["you"] == packet["params"]["move"])
+	if (packet["params"]["you"] == packet["params"]["move"])
 	{
 		document.getElementById("info").innerHTML = "Your move"
 	}
@@ -216,6 +267,9 @@ ttt_websocket.onmessage = function (event) {
 		cellElements.forEach(cell => { cell.removeEventListener('click', make_move)})
 		
 	}
+	function make_move(e)
+	{
+		
 		const cell = e.target
 		
 let x = cell.id % 9
